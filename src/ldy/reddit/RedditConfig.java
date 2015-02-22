be08c2d3fa1294087c7d7f107a03d8e5a3153d2f
Dongyuan <dongyuanlu@gmail.com>
2015-02-22 21:17:41 +0800

    del

--- src/ldy/reddit/RedditConfig.java
+++
@@ -1,8 +0,0 @@
-package ldy.reddit;
-
-public class RedditConfig {
-	
-	public static String videoControversialAPIroot = "http://www.reddit.com/r/videos/controversial/.json?limit=100&t=year&after=";
-	
-
-}
