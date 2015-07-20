0c78d24add1715ea733ed6877a3b84ba4801c083
dongyuan <dongyuanlu@gmail.com>
2015-07-20 16:32:39 +0800

    add comment

--- src/ldy/instagram/InstagramToken.java
+++ src/ldy/instagram/InstagramToken.java
@@ -1,13 +1,19 @@
 package ldy.instagram;
 
-
+/**
+ * 
+ * Select access_token for usage
+ * 
+ * @author Administrator
+ *
+ */
 public class InstagramToken {
 	
 	private String[] tokens = InstagramConfig.accessTokens;
 	private int[] countTokens;
 	private long[] startTime;
 	
-	private int LIMIT = 5000; //instagram visit limit, 5000 / hour
+	private int LIMIT = 5000; //Instagram visit limit, 5000 / hour
 	private long HOUR = 3600000;	//one hour in milliseconds
 	
 	private int currentIndex;
@@ -23,7 +29,13 @@ public class InstagramToken {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * Iteratively pick one valid access_token from InstagramConfig.accessTokens
+	 * Under limitation: 5000 visits per hour
+	 * 
+	 * @return
+	 */
 	public String pickToken(){
 		
 		if(countTokens[currentIndex] < LIMIT)
